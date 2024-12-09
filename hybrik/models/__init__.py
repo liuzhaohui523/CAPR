23b64ed2cee87605772251a6566eb11d568b9cb2
1362388547@qq.com <Songzuer0523@>
2024-12-09 14:42:50 +0800

    删除

--- hybrik/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .HeaterWithCam import HeatERCam
-from .criterion import *  # noqa: F401,F403
-
-__all__ = ["HeatERCam"]
--- hybrik/version.py
+++
@@ -1,5 +0,0 @@
-# GENERATED VERSION FILE
-# TIME: Wed Mar 29 21:37:57 2023
-
-__version__ = '0.1.0+0b6ef9f'
-short_version = '0.1.0'
